e5766d6eb8954d1e5caf675f9bcf62288fcc6fda
awilkins2 <125494114+awilkins2@users.noreply.github.com>
2023-08-29 14:26:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+#syntax is the way we write code
+# print("Hello World")
+# name = john
+#in other language is, this is different
+word = "python lol this is the most stuff"  #string variable
+print(word)
+print(word[-2])  #prints the first letter of the string
+print(word[3])
+#string slicing
+print(word[0:7])
+print(word[28:33])
+print(word[7:10])
+print(word[23:27])
+print(word[-24:-1])
+print(word[10:14])
+print(len(word))  #shows how many characters
+print(word[:13])
+print(word[5:])
+#a substring
+print(word.find("is"))
+#how to upper words
+print(word.upper())
+print(word.capitalize())
+python = word[0:6]
+print(python)
+print(python.capitalize())
+print(word.replace("python", "javascript"))
+print(word)
+print(word.replace("most", "ugly"))
+print(word)
+
+print()
+print()
+
+word = "Jaime Reyes suddenly finds himself in possession of an ancient relic of alien biotechnology called the Scarab. When the Scarab chooses Jaime to be its symbiotic host, he's bestowed with an incredible suit of armor that's capable of extraordinary and unpredictable powers, forever changing his destiny as he becomes the superhero Blue Beetle."
+
+print(word)
+print()
+print()
+print(word.upper())
+print()
+print()
+print(word.lower())
+print()
+print()
+
+print(word.replace("Blue", "red"))
+print()
+print(word)
